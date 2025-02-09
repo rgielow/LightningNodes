c1306d39f93e76df1d6b30af6a887b6cb8f8cab0
rodrigogielow <rodrigo.gielow@southsystem.com.br>
2025-02-09 09:14:04 -0300

    Improved loading and error animations.

--- app/src/main/java/com/satoshi/lightningnodes/feature/error/ErrorScreen.kt
+++ app/src/main/java/com/satoshi/lightningnodes/feature/error/ErrorScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -36,9 +37,13 @@ fun ErrorScreen(error: String, onRetry: () -> Unit) {
             iterations = LottieConstants.IterateForever
         )
         Spacer(modifier = Modifier.padding(8.dp))
-        Text(text = error)
+        Text(modifier = Modifier.padding(horizontal = 16.dp), text = error)
         Spacer(modifier = Modifier.padding(8.dp))
-        Button(onClick = { onRetry() }) {
+        Button(
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .fillMaxWidth(),
+            onClick = { onRetry() }) {
             Text(text = stringResource(R.string.retry))
         }
     }
